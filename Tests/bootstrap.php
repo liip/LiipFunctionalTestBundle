8c9ca663332908a559576b3adf4898d69500935c
Jeanmonod David <david.jeanmonod@gmail.com>
2012-07-13 12:22:05 +0200

    Fix autoloading for standalone tests with composer

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,26 @@
 <?php
 
-$symfonyDir = __DIR__.'/../../../../symfony/src/';
-require_once $symfonyDir.'Symfony/Component/ClassLoader/UniversalClassLoader.php';
+// Autoloading is different according to the context
+$context = file_exists(__DIR__.'/../vendor') ? 'standalone' : 'sf2_project';
+echo "Autoloading type: [$context]\n";
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
+// In standalone execution we rely on composer autoloader
+if ($context === 'standalone'){
+    $file = __DIR__.'/../vendor/autoload.php';
+    if (!file_exists($file)) {
+        throw new RuntimeException('Install dependencies to run test suite.');
+    }
+    $loader = require_once $file;
+    $loader->add('TestBundle', __DIR__.'/Functional/app/src');
+}
 
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $symfonyDir);
-$loader->registerNamespace('Liip', __DIR__.'/../../..');
-$loader->registerNamespace('TestBundle', __DIR__.'/Functional/app/src');
-$loader->register();
+// In symfony project project context, we rely on the symfony class loader 
+else {
+    $symfonyDir = realpath(__DIR__.'/../../../../../vendor/symfony/src'); 
+    require_once $symfonyDir.'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+    $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+    $loader->registerNamespace('Symfony', $symfonyDir);
+    $loader->registerNamespace('Liip', __DIR__.'/../../..');
+    $loader->registerNamespace('TestBundle', __DIR__.'/Functional/app/src');
+    $loader->register();
+}
