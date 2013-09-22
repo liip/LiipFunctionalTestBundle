0118052cf648a0c7519fc1fc2d0eb92c05be4374
Matt Eckerle <mattexx@gmail.com>
2013-09-22 02:40:23 -0700

    Revert "Merge pull request #98 from vishalmelmatti/master"
    This reverts commit 6fad72dd66b54bd85cf77208fad9d20e89f1999f, reversing
    changes made to bd1e7086a4ce06ea0896f4588de454d1aec5480f.
    
    issue #101

--- Test/WebTestCase.php
+++ Test/WebTestCase.php
@@ -37,7 +37,6 @@ use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader as DataFixturesLoader;
 use Symfony\Bundle\DoctrineFixturesBundle\Common\DataFixtures\Loader as SymfonyFixturesLoader;
 use Doctrine\Bundle\FixturesBundle\Common\DataFixtures\Loader as DoctrineFixturesLoader;
-use Doctrine\Common\DataFixtures\Purger\ORMPurger as ORMPurger;
 
 /**
  * @author Lea Haensenberger
@@ -237,10 +236,6 @@ abstract class WebTestCase extends BaseWebTestCase
             $om = $registry->getEntityManager($omName);
             $type = 'ORM';
         }
-        
-        if (($type == 'ORM') && ($purgeMode === null)) {
-            $purgeMode = ORMPurger::PURGE_MODE_TRUNCATE;
-        }        
 
         $executorClass = 'Doctrine\\Common\\DataFixtures\\Executor\\'.$type.'Executor';
         $referenceRepository = new ProxyReferenceRepository($om);
