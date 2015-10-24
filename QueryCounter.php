dd1f101398abad36676fb4fff7d1e55ffb431813
Lukas Kahwe Smith <smith@pooteeweet.org>
2015-10-25 00:22:42 +0200

    minor CS tweak

--- QueryCounter.php
+++ QueryCounter.php
@@ -55,7 +55,7 @@ class QueryCounter
                 );
 
                 foreach ($annotations as $annotationClass) {
-                    if ($annotationClass instanceof QueryCount and isset($annotationClass->maxQueries)) {
+                    if ($annotationClass instanceof QueryCount && isset($annotationClass->maxQueries)) {
                         /* @var $annotations \Liip\FunctionalTestBundle\Annotations\QueryCount */
 
                         return $annotationClass->maxQueries;
