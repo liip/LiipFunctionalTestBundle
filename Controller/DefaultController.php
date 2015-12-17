39d450b883e11c76defd5b67bcab5182dc75ae8d
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2015-12-17 23:56:22 +0100

    Fixes 197: Added tests and Continuous Integration services:
    Added test environment:
     - AppKernel with configuration and route (see http://www.whitewashing.de/2012/02/25/symfony2_controller_testing.html for reference)
     - Twig view
     - Simple test file with fake authentification
     - Dependencies in composer.json: Symfony and PHPUnit
    
    Added DataFixtures and tests with fixtures
    Added User entity (I had to use the YAML format for Doctrine, annotations didn't worked)
    Web tests: added a form with validation errors assertions, added status code assertions
    Added a simple command and a test for it
    Replaced new array syntax with PHP5.3-compatible array syntax (see #194)
    Travis CI: Symfony 2.3 is in allowed failures
    Scrutinizer: Composer will install Symfony 2.8.*
    
    Added badges in README.md: Travis CI, Scrutinizer and SensioLabsInsight

---
+++ Controller/DefaultController.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Liip/FunctionalTestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+ 
+namespace Liip\FunctionalTestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DefaultController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return($this->render(
+            'LiipFunctionalTestBundle::layout.html.twig'
+        ));
+    }
+    
+    /**
+     * @param integer $userId
+     * 
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function userAction($userId)
+    {
+        $user = $this->getDoctrine()
+            ->getRepository('LiipFunctionalTestBundle:User')
+            ->find($userId);
+        
+        if (! $user)
+		{
+			throw $this->createNotFoundException(
+				'No user found'
+			);
+		}
+        
+        return($this->render(
+            'LiipFunctionalTestBundle:Default:user.html.twig',
+            array('user' => $user)
+        ));
+    }
+    
+    /**
+     * @param Request $request
+     * 
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function formAction(Request $request)
+    {
+        $object = new \ArrayObject();
+        $object->name = null;
+        
+        $form = $this->createFormBuilder($object)
+            ->add('name', 'text')
+            ->add('Submit', 'submit')
+            ->getForm();
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->get('session')->getFlashBag()->add('notice',
+                'Name submitted.'
+            );
+        }
+        
+        return $this->render('LiipFunctionalTestBundle:Default:form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
---
+++ DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Liip/FunctionalTestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Liip\FunctionalTestBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Liip\FunctionalTestBundle\Entity\User;
+
+class LoadUserData extends AbstractFixture
+    implements OrderedFixtureInterface, FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('foo bar');
+        $user->setEmail('foo@bar.com');
+        // Set according to your security context settings
+        $encoder = new MessageDigestPasswordEncoder('sha1', true, 3);
+        $user->setPassword($encoder->encodePassword('12341234', $user->getSalt()));
+        $user->setAlgorithm('sha1');
+        $user->setEnabled(true);
+        $user->setConfirmationToken(null);
+
+        $manager->persist($user);
+        $manager->flush();
+        
+        $this->addReference('user', $user);
+        
+        $user = clone $this->getReference('user');
+        
+        $user->setId(2);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
