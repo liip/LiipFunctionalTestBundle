1946e53022250b490550a394423b2185668b0594
Lukas Kahwe Smith <smith@pooteeweet.org>
2016-12-14 21:48:34 +0100

    add phpunit bridge to fix deprecation warnings with symfony 2.7

--- Tests/App/bootstrap.php
+++ Tests/App/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Bridge\PhpUnit\DeprecationErrorHandler;
 
 $loader = require __DIR__.'/../../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+DeprecationErrorHandler::register(getenv('SYMFONY_DEPRECATIONS_HELPER'));
 
 return $loader;
