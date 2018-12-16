fdbc8866e733870f88f05eec4cd16b6bb26c9fe1
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2018-12-16 16:59:09 +0100

    2.x: Add tests with MySQL database cache

---
+++ tests/AppConfigMysqlCacheDb/AppConfigMysqlKernelCacheDb.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Liip/FunctionalTestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Liip\FunctionalTestBundle\Tests\AppConfigMysqlCacheDb;
+
+/*
+ * This file is part of the Liip/FunctionalTestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Liip\FunctionalTestBundle\Tests\AppConfigMysql\AppConfigMysqlKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppConfigMysqlKernelCacheDb extends AppConfigMysqlKernel
+{
+    /**
+     * Load the config.yml from the current directory.
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        // Load the default file.
+        parent::registerContainerConfiguration($loader);
+
+        // Load the file with MySQL configuration
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
---
+++ tests/Test/WebTestCaseConfigMysqlCacheDbTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Liip/FunctionalTestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Liip\FunctionalTestBundle\Tests\Test;
+
+use Liip\FunctionalTestBundle\Tests\AppConfigMysqlCacheDb\AppConfigMysqlKernelCacheDb;
+
+/**
+ * Test MySQL database with database caching enabled.
+ *
+ * The following tests require a connection to a MySQL database,
+ * they are disabled by default (see phpunit.xml.dist).
+ *
+ * In order to run them, you have to set the MySQL connection
+ * parameters in the Tests/AppConfigMysql/config.yml file and
+ * add “--exclude-group ""” when running PHPUnit.
+ *
+ * Use Tests/AppConfigMysql/AppConfigMysqlKernelCacheDb.php instead of
+ * Tests/App/AppKernel.php.
+ * So it must be loaded in a separate process.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class WebTestCaseConfigMysqlCacheDbTest extends WebTestCaseConfigMysqlTest
+{
+    protected static function getKernelClass(): string
+    {
+        return AppConfigMysqlKernelCacheDb::class;
+    }
+}
